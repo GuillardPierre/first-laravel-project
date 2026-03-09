9f6a378d03a7121c9cc46863c35f6527df0fcf68
PierreGllrd <pguillard95@gmail.com>
2026-03-09 16:50:32 +0100

    Activité 8

--- routes/web.php
+++ routes/web.php
@@ -7,3 +7,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [IndexController::class, 'index'])->name("home");
 Route::get("/articles/{id}", [ArticleController::class, 'show'])->name("article.details");
 
+Route::fallback(function () {
+    return view("errors.not-found");
+});
