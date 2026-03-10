6d6cbb91d9c4428aef4001a0149a2aca7c8fa7ad
PierreGllrd <pguillard95@gmail.com>
2026-03-10 15:10:43 +0100

    Correction activité 13

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,9 +4,10 @@
     <h2>Bienvenu sur le site de {{ $name }}</h2>
 
     @forelse ($articles as $article) 
-    @if (!$loop->last)
-    <x-article :title="$article['title']" :content="$article['content']" />
+    @if ($loop->last)
+    @break
     @endif
+    <x-article :title="$article['title']" :content="$article['content']" />
     @empty
         <p>Aucun article pour le moment.</p>
     @endforelse
