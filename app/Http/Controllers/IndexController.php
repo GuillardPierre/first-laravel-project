af4e1ae4512637fc5789be5f1e33565ab9e80c5f
PierreGllrd <pguillard95@gmail.com>
2026-03-10 14:26:50 +0100

    activité 12

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -8,6 +8,15 @@ class IndexController extends Controller
 {
     public function index()
     {
-        return view("welcome", ["name" => "Pierre"]);
+        $articles = [
+            ["title" => "L’IA soigne mieux", "content" => "L’intelligence artificielle aide les médecins à diagnostiquer plus vite."],
+            ["title" => "Villes verte", "content" => "Les métropoles deviennent plus écologiques et durables."],
+            ["title" => "Télétravail", "content" => "Plus de liberté, mais aussi plus de solitude."],
+        ];
+
+        return view("welcome", [
+            "name" => "Pierre",
+            "articles" => $articles,
+        ]);
     }
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,9 +2,12 @@
 @section('title', 'Accueil')
 @section('content')
     <h2>Bienvenu sur le site de {{ $name }}</h2>
-    <x-article title="L’IA soigne mieux" content="L’intelligence artificielle aide les médecins à diagnostiquer plus vite."/>
-    <x-article title="Villes verte" content="Les métropoles deviennent plus écologiques et durables."/>
-    <x-article title="Télétravail" content="Plus de liberté, mais aussi plus de solitude."/>
+
+    @forelse ($articles as $article)
+        <x-article :title="$article['title']" :content="$article['content']" />
+    @empty
+        <p>Aucun article pour le moment.</p>
+    @endforelse
 @endsection
 
 
