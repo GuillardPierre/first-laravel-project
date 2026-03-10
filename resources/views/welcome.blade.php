8b944e5de2c5e30e42e2d4f7aa93bbca98d578c5
PierreGllrd <pguillard95@gmail.com>
2026-03-10 13:54:31 +0100

    activité 11

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,6 +2,9 @@
 @section('title', 'Accueil')
 @section('content')
     <h2>Bienvenu sur le site de {{ $name }}</h2>
+    <x-article title="L’IA soigne mieux" content="L’intelligence artificielle aide les médecins à diagnostiquer plus vite."/>
+    <x-article title="Villes verte" content="Les métropoles deviennent plus écologiques et durables."/>
+    <x-article title="Télétravail" content="Plus de liberté, mais aussi plus de solitude."/>
 @endsection
 
 
