7db9afbe347edd8135bf4c710239fc35628ae930
PierreGllrd <pguillard95@gmail.com>
2026-03-10 14:58:16 +0100

    Activité 12

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,8 +3,10 @@
 @section('content')
     <h2>Bienvenu sur le site de {{ $name }}</h2>
 
-    @forelse ($articles as $article)
-        <x-article :title="$article['title']" :content="$article['content']" />
+    @forelse ($articles as $article) 
+    @if (!$loop->last)
+    <x-article :title="$article['title']" :content="$article['content']" />
+    @endif
     @empty
         <p>Aucun article pour le moment.</p>
     @endforelse
